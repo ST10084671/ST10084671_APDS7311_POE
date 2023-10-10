193235d209a7f34ba0c6b943bc353a05ce6ccf57
Ammarah-Kader <ST10084671@vcconnect.edu.za>
2023-10-10 15:34:25 +0200

    initial commit five

--- server/routes/record.mjs
+++ server/routes/record.mjs
@@ -4,7 +4,7 @@ import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
-// This section will help you get a list of all the records
+// This section will help you to get a list of all the records
 router.get("/", async (req, res) => 
 {
   let collection = await db.collection("records");
@@ -12,7 +12,7 @@ router.get("/", async (req, res) =>
   res.send(results).status(200);
 });
 
-// This section will help you get a single record by id
+// This section will help you to get a single record by the id
 router.get("/:id", async (req, res) => 
 {
   let collection = await db.collection("records");
@@ -23,7 +23,7 @@ router.get("/:id", async (req, res) =>
   else res.send(result).status(200);
 });
 
-// This section will help you create a new record
+// This section will help you to create a new record
 router.post("/", async (req, res) => 
 {
   let newDocument = 
@@ -37,7 +37,7 @@ router.post("/", async (req, res) =>
   res.send(result).status(204);
 });
 
-// This section will help you update a record by id
+// This section will help you to update a record by the id
 router.patch("/:id", async (req, res) => 
 {
   const query = { _id: new ObjectId(req.params.id) };
@@ -57,7 +57,7 @@ router.patch("/:id", async (req, res) =>
   res.send(result).status(200);
 });
 
-// This section will help you delete a record
+// This section will help you to delete a record
 router.delete("/:id", async (req, res) => 
 {
   const query = { _id: new ObjectId(req.params.id) };
